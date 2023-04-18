e64c1ef1a263231342658aea6f8b39c7cf2e572d
Nick GS <gsidnick@gmail.com>
2023-04-18 22:08:48 +0300

    refactor: add attribute in index.html and app.js according to rule 1.2 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="button delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.alt='Delete';
     deleteButtonImg.className='icon';
     deleteButton.appendChild(deleteButtonImg);
 
